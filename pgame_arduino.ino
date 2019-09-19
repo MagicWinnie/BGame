fe717964e35b54dcfd906dda624f4ec6181aa60f
Dmitrii <39788810+MagicWinnie@users.noreply.github.com>
2019-09-19 23:38:54 +0700

    Add files via upload

---
+++ pgame_arduino.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  Serial.print("#");
+  Serial.print(analogRead(A0));
+  Serial.print(" ");
+  Serial.print(analogRead(A1));
+  Serial.println("%");
+  delay(20);
+}
